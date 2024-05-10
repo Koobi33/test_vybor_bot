d0ea114ff2482b689ae8533b6208af5b94a8caaa
Suchkov Dmitry <45110271+Koobi33@users.noreply.github.com>
2024-05-10 09:47:02 -0300

    webapp link

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
-export const WEB_APP_URL = "https://t.me/wouldyourather_game_bot/play";
+// export const WEB_APP_URL = "https://t.me/wouldyourather_game_bot/play";
+export const WEB_APP_URL = "https://test-vybor.vercel.app";
 export const START_GIF_LINK =
   "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjlkdXlzaG83c3IxN3lnYjI5bXF0cjNtN3ZsdXBuZW9nODFyOXZ0dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3tpV6MjJnrwLHmAzgq/giphy.gif";
 export const TELEGRAM_CHANNEL_RU = "https://t.me/wouldyourather_ru";
